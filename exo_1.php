a6c5a3c17b72ed8938f6c179ce34834f9d40b7a5
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:00:03 +0100

    commit

---
+++ exo_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>exercice 1</title>
+</head>
+<body>
+	<?php
+		$name="Ceci est une variable de mon choix";
+ 		echo "Bonjour le monde $name";
+	?>
+
+</body>
+</html>
